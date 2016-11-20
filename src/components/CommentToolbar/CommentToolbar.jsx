feea9d636a4723e5bfd8ffb7310f48fa9affac0d
choochoocho <choochocho@outlook.com>
2016-11-20 13:44:49 +0300

    add CommentToolbar

---
+++ src/components/CommentToolbar/CommentToolbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+
+export default class CommentToolbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 3,
+    };
+  }
+
+  handleChange = (event, index, value) => this.setState({value});
+
+  render() {
+    return (
+      <Toolbar>
+        <ToolbarGroup>
+            <ToolbarTitle text="Обсуждение" />
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <RaisedButton label="Написать комментарий" primary={true} />
+        </ToolbarGroup>
+      </Toolbar>
+    );
+  }
+}
