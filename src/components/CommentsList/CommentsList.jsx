40ca8421517686fd20933255641fe87f747a92f1
choochoocho <choochocho@outlook.com>
2016-11-20 13:45:02 +0300

    add CommentList

---
+++ src/components/CommentsList/CommentsList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CommentsList.sass';
+import Comment from '../Comment/Comment.jsx';
+
+export default class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="comments-list">
+                {
+                    this.props.items.map(comment => {
+                        return <Comment key={comment.id} { ...comment } />;
+                    })
+                }
+            </div>
+        );
+    }
+}
